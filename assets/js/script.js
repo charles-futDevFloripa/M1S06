f6c930b59a10fc23aaca459480dd8f761dd8fa91
cbiveu <cbiveu@gmail.com>
2024-03-21 16:23:21 -0300

    [M1S06] - Ex 8 - Manipulação do DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,4 @@
-alert('Bem vindo ao meu profile');
+document.addEventListener('DOMContentLoaded', function () {
+  alert('Bem vindo ao meu profile');
+  document.getElementById('profile-js').innerText = 'In Home Office since 1997';
+});
